596c58e7e340f094ff6afb484b122781f0014ead
Paradisumal <55985977+Paradisumal@users.noreply.github.com>
2020-10-02 10:54:48 +0100

    Created Products (Index) by Category View, linked StoreController Category Index.

--- Producks.Web/Controllers/StoreController.cs
+++ Producks.Web/Controllers/StoreController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,13 @@ namespace Producks.Web.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Categories.ToListAsync());
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Products([FromQuery, Required] int categoryId)
+        {
+            var products = _context.Products.Where(p => p.CategoryId == categoryId).Include(p => p.Brand);
+            return View(await products.ToListAsync());
         }
     }
 }
