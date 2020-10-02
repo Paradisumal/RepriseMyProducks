430484df5af9939daa85f7e750d2f928c95269bc
Paradisumal <55985977+Paradisumal@users.noreply.github.com>
2020-10-02 10:24:16 +0100

    Created StoreController and Category Index View for it.

---
+++ Producks.Web/Controllers/StoreController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Producks.Data;
+
+namespace Producks.Web.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly StoreDb _context;
+
+        public StoreController(StoreDb context)
+        {
+            _context = context;
+        }
+
+        // GET: Categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+    }
+}
