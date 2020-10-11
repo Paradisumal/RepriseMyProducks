836fe8f9c0c8efb7c19eec018448440ade6222c0
Paradisumal <55985977+Paradisumal@users.noreply.github.com>
2020-10-11 16:07:32 +0100

    Created Undercutter Dto's for Product, Brand and Category.

---
+++ Producks.Web/Models/UCBrandDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Producks.Web.Models
+{
+    public class UCBrandDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AvailableProductCount { get; set; }
+    }
+}
---
+++ Producks.Web/Models/UCProductDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Producks.Web.Models
+{
+    public class UCProductDto
+    {
+        public int Id { get; set; }
+        public string Ean { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool InStock { get; set; }
+        [DataType(DataType.Date)]
+        public string ExpectedRestock { get; set; }
+    }
+}
